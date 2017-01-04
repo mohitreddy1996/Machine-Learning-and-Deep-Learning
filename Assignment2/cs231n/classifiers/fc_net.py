e90248fb019552e5cd3c5cc76c72eaaa31d52b01
Mohit Reddy <mohitreddy1996@gmail.com>
2017-01-04 09:04:31 +0530

    Fully Connected Neural Nets started

--- Assignment2/cs231n/classifiers/fc_net.py
+++ Assignment2/cs231n/classifiers/fc_net.py
@@ -45,7 +45,10 @@ class TwoLayerNet(object):
     # weights and biases using the keys 'W1' and 'b1' and second layer weights #
     # and biases using the keys 'W2' and 'b2'.                                 #
     ############################################################################
-    pass
+    self.params['W1'] = np.random.normal(scale = weight_scale, size=(input_dim, hidden_dim))
+    self.params['W2'] = np.random.normal(scale = weight_scale, size=(hidden_dim, num_classes))
+    self.params['b1'] = np.zeros(hidden_dim)
+    self.params['b2'] = np.zeros(num_classes)
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
@@ -75,7 +78,8 @@ class TwoLayerNet(object):
     # TODO: Implement the forward pass for the two-layer net, computing the    #
     # class scores for X and storing them in the scores variable.              #
     ############################################################################
-    pass
+    out1, cache1 = affine_relu_forward(X, self.params['W1'], self.params['b1'])
+    scores, cache2 = affine_forward(out1, self.params['W2'], self.params['b2'])
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
@@ -95,7 +99,17 @@ class TwoLayerNet(object):
     # automated tests, make sure that your L2 regularization includes a factor #
     # of 0.5 to simplify the expression for the gradient.                      #
     ############################################################################
-    pass
+    loss, dscores = softmax_loss(scores, y)
+    # print " loss after softmax : " + str(loss)
+    loss += (0.5*self.reg*np.sum(self.params['W1']**2) + 0.5*self.reg*np.sum(self.params['W2']**2))
+    # print " loss after regularization : " + str(loss)
+    # backward prop.
+    dx2, grads['W2'], grads['b2'] = affine_backward(dscores, cache2)
+    dx1, grads['W1'], grads['b1'] = affine_relu_backward(dx2, cache1)
+
+    # update the grads by regularization term
+    grads['W1'] += (0.5*self.reg*self.params['W1'])
+    grads['W2'] += (0.5*self.reg*self.params['W2'])
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
